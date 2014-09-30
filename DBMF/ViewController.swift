8cdcb4da6ca6ad02da02e41b2c4e55ea2e0e9367
august-zou <zjzouxu@gmail.com>
2014-09-30 15:07:58 +0800

    Initial Commit

---
+++ DBMF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBMF
+//
+//  Created by august-mba on 14-9-30.
+//  Copyright (c) 2014年 heletech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
